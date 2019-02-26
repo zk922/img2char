ab1fa4edcc4fad6c6e3b9b7c7603b1217d266ac9
zhangkuo <zhangk922@163.com>
2019-02-26 16:57:14 +0800

    搭建测试服务器

--- test/test-server.ts
+++ test/test-server.ts
@@ -2,20 +2,34 @@ import * as Koa from "koa";
 import * as Router from "koa-router";
 import * as cp from "child_process";
 import * as path from "path";
+import * as koaStatic from "koa-static";
+import * as bodyParser from "koa-bodyparser";
 import image2char from "../src/img2char";
 
 let app: Koa = new Koa();
+
+app.use(koaStatic(path.resolve(__dirname, './www'), {maxAge: 7*24*60*60*1000}));
+
+app.use(bodyParser({
+  enableTypes: ['json', 'form', 'text']
+}));
+
 let router = new Router();
-router.get('/', async function (ctx, next) {
+router.post('/convert', async function (ctx, next) {
+
+  let params = ctx.request.body;
+  // ctx.response.set('content-type', 'application/json');
+  if(params.outputMode === 'char'){
+    ctx.response.set('content-type', 'text/plain');
+  }
+  else if(params.outputMode === 'img'){
+    ctx.response.set('content-type', 'image/png');
+  }
+  ctx.body = await image2char(params.img, {outputMode: params.outputMode});
   ctx.status = 200;
-  ctx.response.set('content-type', 'application/json');
-
-
-  ctx.body = await image2char(path.resolve(__dirname, '../timg.jpg'));
-
-
   await next();
 });
+
 
 app.use(router.routes());
 
