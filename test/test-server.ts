094cd55aced4fa631d98195c0690c4fdc0b39b52
zhangkuo <zhangk922@163.com>
2019-02-26 19:50:09 +0800

    koabody

--- test/test-server.ts
+++ test/test-server.ts
@@ -3,29 +3,35 @@ import * as Router from "koa-router";
 import * as cp from "child_process";
 import * as path from "path";
 import * as koaStatic from "koa-static";
-import * as bodyParser from "koa-bodyparser";
+import * as koaBody from "koa-body";
+import * as fs from "fs";
 import image2char from "../src/img2char";
 
 let app: Koa = new Koa();
 
 app.use(koaStatic(path.resolve(__dirname, './www'), {maxAge: 7*24*60*60*1000}));
 
-app.use(bodyParser({
-  enableTypes: ['json', 'form', 'text']
+app.use(koaBody({
+  multipart: true,
+  formidable:{
+    keepExtensions: true,
+    maxFieldsSize:2 * 1024 * 1024,
+  }
 }));
-
 let router = new Router();
 router.post('/convert', async function (ctx, next) {
 
   let params = ctx.request.body;
-  // ctx.response.set('content-type', 'application/json');
+  let files = ctx.request.files;
   if(params.outputMode === 'char'){
     ctx.response.set('content-type', 'text/plain; charset=utf-8');
   }
   else if(params.outputMode === 'img'){
     ctx.response.set('content-type', 'image/png; charset=utf-8');
   }
-  ctx.body = await image2char(params.img, {outputMode: params.outputMode});
+  ctx.body = await image2char(files.img.path, {outputMode: params.outputMode});
+  // ctx.body = JSON.stringify(img)
+  fs.unlinkSync(files.img.path);
   ctx.status = 200;
   await next();
 });
